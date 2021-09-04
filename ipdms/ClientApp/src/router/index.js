7430d92b983a16f73add9c0df434fc127dd14853
Kevin Mendi <kevin.mendi20@gmail.com>
2021-09-04 23:54:11 +0800

    Renaming Pages

--- ipdms/ClientApp/src/router/index.js
+++ ipdms/ClientApp/src/router/index.js
@@ -14,7 +14,7 @@ export default new Router({
         {
             path: '/',
             name: 'analytics',
-            component: () => import('../DemoPages/Dashboards/Analytics.vue'),
+            component: () => import('../Pages/Dashboards/Analytics.vue'),
         },
 
         // Pages
@@ -23,19 +23,19 @@ export default new Router({
             path: '/pages/login-boxed',
             name: 'login-boxed',
             meta: {layout: 'userpages'},
-            component: () => import('../DemoPages/UserPages/LoginBoxed.vue'),
+            component: () => import('../Pages/UserPages/LoginBoxed.vue'),
         },
         {
             path: '/pages/register-boxed',
             name: 'register-boxed',
             meta: {layout: 'userpages'},
-            component: () => import('../DemoPages/UserPages/RegisterBoxed.vue'),
+            component: () => import('../Pages/UserPages/RegisterBoxed.vue'),
         },
         {
             path: '/pages/forgot-password-boxed',
             name: 'forgot-password-boxed',
             meta: {layout: 'userpages'},
-            component: () => import('../DemoPages/UserPages/ForgotPasswordBoxed.vue'),
+            component: () => import('../Pages/UserPages/ForgotPasswordBoxed.vue'),
         },
 
         // Elements
@@ -43,42 +43,42 @@ export default new Router({
         {
             path: '/elements/buttons-standard',
             name: 'buttons-standard',
-            component: () => import('../DemoPages/Elements/Buttons/Standard.vue'),
+            component: () => import('../Pages/Elements/Buttons/Standard.vue'),
         },
         {
             path: '/elements/dropdowns',
             name: 'dropdowns',
-            component: () => import('../DemoPages/Elements/Dropdowns.vue'),
+            component: () => import('../Pages/Elements/Dropdowns.vue'),
         },
         {
             path: '/elements/icons',
             name: 'icons',
-            component: () => import('../DemoPages/Elements/Icons.vue'),
+            component: () => import('../Pages/Elements/Icons.vue'),
         },
         {
             path: '/elements/badges-labels',
             name: 'badges',
-            component: () => import('../DemoPages/Elements/Badges.vue'),
+            component: () => import('../Pages/Elements/Badges.vue'),
         },
         {
             path: '/elements/cards',
             name: 'cards',
-            component: () => import('../DemoPages/Elements/Cards.vue'),
+            component: () => import('../Pages/Elements/Cards.vue'),
         },
         {
             path: '/elements/list-group',
             name: 'list-group',
-            component: () => import('../DemoPages/Elements/ListGroups.vue'),
+            component: () => import('../Pages/Elements/ListGroups.vue'),
         },
         {
             path: '/elements/timelines',
             name: 'timeline',
-            component: () => import('../DemoPages/Elements/Timeline.vue'),
+            component: () => import('../Pages/Elements/Timeline.vue'),
         },
         {
             path: '/elements/utilities',
             name: 'utilities',
-            component: () => import('../DemoPages/Elements/Utilities.vue'),
+            component: () => import('../Pages/Elements/Utilities.vue'),
         },
 
         // Components
@@ -86,42 +86,42 @@ export default new Router({
         {
             path: '/components/tabs',
             name: 'tabs',
-            component: () => import('../DemoPages/Components/Tabs.vue'),
+            component: () => import('../Pages/Components/Tabs.vue'),
         },
         {
             path: '/components/accordions',
             name: 'accordions',
-            component: () => import('../DemoPages/Components/Accordions.vue'),
+            component: () => import('../Pages/Components/Accordions.vue'),
         },
         {
             path: '/components/modals',
             name: 'modals',
-            component: () => import('../DemoPages/Components/Modals.vue'),
+            component: () => import('../Pages/Components/Modals.vue'),
         },
         {
             path: '/components/progress-bar',
             name: 'progress-bar',
-            component: () => import('../DemoPages/Components/ProgressBar.vue'),
+            component: () => import('../Pages/Components/ProgressBar.vue'),
         },
         {
             path: '/components/tooltips-popovers',
             name: 'tooltips-popovers',
-            component: () => import('../DemoPages/Components/TooltipsPopovers.vue'),
+            component: () => import('../Pages/Components/TooltipsPopovers.vue'),
         },
         {
             path: '/components/carousel',
             name: 'carousel',
-            component: () => import('../DemoPages/Components/Carousel.vue'),
+            component: () => import('../Pages/Components/Carousel.vue'),
         },
         {
             path: '/components/pagination',
             name: 'pagination',
-            component: () => import('../DemoPages/Components/Pagination.vue'),
+            component: () => import('../Pages/Components/Pagination.vue'),
         },
         {
             path: '/components/maps',
             name: 'maps',
-            component: () => import('../DemoPages/Components/Maps.vue'),
+            component: () => import('../Pages/Components/Maps.vue'),
         },
 
         // Tables
@@ -129,7 +129,7 @@ export default new Router({
         {
             path: '/tables/regular-tables',
             name: 'regular-tables',
-            component: () => import('../DemoPages/Tables/RegularTables.vue'),
+            component: () => import('../Pages/Tables/RegularTables.vue'),
         },
 
         // Dashboard Widgets
@@ -137,7 +137,7 @@ export default new Router({
         {
             path: '/widgets/chart-boxes-3',
             name: 'chart-boxes-3',
-            component: () => import('../DemoPages/Widgets/ChartBoxes3.vue'),
+            component: () => import('../Pages/Widgets/ChartBoxes3.vue'),
         },
 
         // Forms
@@ -145,19 +145,19 @@ export default new Router({
         {
             path: '/forms/controls',
             name: 'controls',
-            component: () => import('../DemoPages/Forms/Elements/Controls.vue'),
+            component: () => import('../Pages/Forms/Elements/Controls.vue'),
         },
         {
             path: '/forms/layouts',
             name: 'layouts',
-            component: () => import('../DemoPages/Forms/Elements/Layouts.vue'),
+            component: () => import('../Pages/Forms/Elements/Layouts.vue'),
         },
         // Charts
 
         {
             path: '/charts/chartjs',
             name: 'chartjs',
-            component: () => import('../DemoPages/Charts/Chartjs.vue'),
+            component: () => import('../Pages/Charts/Chartjs.vue'),
         },
     ]
 })
